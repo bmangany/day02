b2372bee3000b86d4fb278f3ffc4de3dc8ebfabc
Bobdow Tiyiselani Manganyi <bmangany@c5r6s3.wethinkcode.co.za>
2020-06-21 14:49:39 +0200

    day02

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmangany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 10:26:58 by bmangany          #+#    #+#             */
+/*   Updated: 2020/06/20 10:30:13 by bmangany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet()
+{
+	char i;
+	i = 'a';
+	
+	while(i <= 'z')
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
+
+int	main()
+{
+	ft_print_alphabet();
+	return (0);
+}
+
